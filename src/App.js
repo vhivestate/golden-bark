94af015a1975cd6dd15b58a9d4830ece58cb8217
vhivestate <garcia.valeria001@gmail.com>
2023-08-19 03:12:35 -0600

    react router on components and pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import Footer from './Components/Footer';
+import Nav from './Components/Nav';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+
+
+      </Routes>
+      <Footer/>
+    </Router>
+  )
 }
 
-export default App;
+export default App
